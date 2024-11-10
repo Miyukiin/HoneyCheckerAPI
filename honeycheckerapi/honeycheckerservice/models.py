756afc4af800c40113da8230662065a8c1261257
Miyukiin <jr.concha99@gmail.com>
2024-11-10 13:31:16 +0800

    Added tables and registered for honeycheckerservice.

--- honeycheckerapi/honeycheckerservice/models.py
+++ honeycheckerapi/honeycheckerservice/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class HoneyCheckerTable(models.Model):
+    class Meta:
+        verbose_name = "HoneyChecker"  # Singular name
+        verbose_name_plural = "HoneyChecker"  # Correct plural name
+        
+    user_random_index = models.IntegerField(_("User Random Index"),unique=True, null=True, blank=True)
+    user_sugarword_index = models.IntegerField(_("User Sugarword Index"), null=True, blank=True)
+    
+    def __str__(self):
+        return f"Honey Index for {self.user_random_index} is {self.user_sugarword_index}"
